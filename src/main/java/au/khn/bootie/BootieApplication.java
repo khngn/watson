faec21e658303002c9ca67dc88fea671530dec9c
KimHai Nguyen <kimhai@gmail.com>
2018-10-20 23:28:38 +1100

    init

---
+++ src/main/java/au/khn/bootie/BootieApplication.java
@@ -0,0 +1,12 @@
+package au.khn.bootie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootieApplication.class, args);
+	}
+}
